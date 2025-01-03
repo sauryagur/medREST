668d60746aacd8ee1b855774019632fad4b25de2
Saurya Gur <saurya.gur@outlook.com>
2025-01-04 02:25:20 +0530

    Revert "Initialised appointments endpoint"
    This reverts commit 021958be5a3c84a89a1579176fb3bb45d212be01.

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,52 +1,52 @@
-import {createClient} from 's'
-
-const supabaseUrl = 'https://cyrokafeqtyphlvgdpbp.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
+const data = require("../data/integer_memory_store.js")
 
 class Pages {
-    constructor(express, next) {
-        this.express = express
-        this.next = next
-    }
+  constructor(express, next) {
+    this.express = express
+    this.next = next
+  }
 
-    init() {
-        this.initCustomPages()
-        this.initDefaultPages()
-    }
+  init() {
+    this.initCustomPages()
+    this.initDefaultPages()
+  }
 
-    initCustomPages() {
-        /* With a monolith api+frontend, it's possible to serve pages with preloaded data */
+  initCustomPages() {
+    /* With a monolith api+frontend, it's possible to serve pages with preloaded data */
+    this.express.get('/preload_data', (req, res) => {
+      res.pageParams = {
+        value: data.value
+      }
+      return this.next.render(req, res, `/preload_data`)
+    })
 
-        /* Special-purpose routing example */
-        this.express.get('/large_or_small/:special_value', (req, res) => {
-            const intValue = parseInt(req.params.special_value)
-            if (isNaN(intValue)) {
-                return this.next.render(req, res, `/invalid_value`, req.query)
-            }
-            if (intValue < 5) {
-                return this.next.render(req, res, `/special_small`, req.query)
-            } else {
-                return this.next.render(req, res, `/special_large`, req.query)
-            }
-        });
+    /* Special-purpose routing example */
+    this.express.get('/large_or_small/:special_value', (req, res) => {
+      const intValue = parseInt(req.params.special_value)
+      if(isNaN(intValue)) {
+        return this.next.render(req, res, `/invalid_value`, req.query)
+      }
+      if(intValue < 5) {
+        return this.next.render(req, res, `/special_small`, req.query)
+      } else {
+        return this.next.render(req, res, `/special_large`, req.query)
+      }
+    })
 
-        this.express.get('/appointments', (req, res) => {
-            const data = [];
+    this.express.get('/appointments', (req, res) => {
+      return.this.next.render(req, res, `/appointments`, data)
+    })
+  }
 
-        })
-    }
+  initDefaultPages() {
+    this.express.get('/', (req, res) => {
+      return this.next.render(req, res, `/main`, req.query)
+    })
 
-    initDefaultPages() {
-        this.express.get('/', (req, res) => {
-            return this.next.render(req, res, `/main`, req.query)
-        })
-
-        this.express.get('*', (req, res) => {
-            return this.next.render(req, res, `${req.path}`, req.query)
-        })
-    }
+    this.express.get('*', (req, res) => {
+      return this.next.render(req, res, `${req.path}`, req.query)
+    })
+  }
 }
 
 module.exports = Pages
