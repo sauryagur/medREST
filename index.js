f828665743ddcab4116ad729c1ed5abfb2f80e8d
Saurya Gur <saurya.gur@outlook.com>
2025-01-09 23:37:07 +0530

    Initialised OPD table autogeneration script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
 import passport from "passport";
-import { Strategy } from "passport-local";
+import {Strategy} from "passport-local";
 import session from "express-session";
 import env from "dotenv";
 
@@ -14,7 +14,7 @@ const app = express();
 const port = 3000;
 const saltRounds = 10;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -27,5 +27,4 @@ app.get("/appointments", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-    console.log(appointments);
 });
