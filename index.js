d71f9061050a6e9467c4bf7ec949bb612e9020d5
jaiveer077 <jaiveersagoo@gmail.com>
2025-01-16 09:33:27 +0530

    Added vercel.json

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback",
-}, (accessToken, refreshToken, profile, done) => {
+}, 
+function (accessToken, refreshToken, profile, done) {
     return done(null, profile);
 }));
 
